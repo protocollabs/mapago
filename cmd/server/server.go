4a0c88371b6dfd6988f25f02901391aae0f3ad90
monfron <simon.fronhoefer@gmail.com>
2018-10-23 17:28:12 +0200

    Added: Basic Dummy JSON creation and coding as binary data
    - LOT of debug code going on
    - Not finished atm
    
    Signed-off-by: monfron <simon.fronhoefer@gmail.com>

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,7 +2,10 @@ package main
 
 import "fmt"
 import "flag"
-import "github.com/monfron/mapago/ctrl/serverProtos"
+import "encoding/json"
+import "encoding/binary"
+import "os"
+// WIP removed during JSON test import "github.com/monfron/mapago/ctrl/serverProtos"
 import "github.com/monfron/mapago/ctrl/shared"
 
 var CTRL_PORT = 64321
@@ -27,13 +30,17 @@ func runServer(port int, callSize int) {
 	ch := make(chan shared.ChResult)
 	fmt.Println(ch)
 
+	/* WIP removed during JSON test
 	tcpObj := serverProtos.NewTcpObj("TcpConn1", port, callSize)
 	tcpObj.Start(ch)
+	*/
 
 	/* WIP: disabled for reduced complexity
 	udpObj := serverProtos.NewUdpObj("UdpConn1")
 	udpObj.Start(ch)
 	*/
+
+	constructDummyJson()
 
 	/* WIP removed during JSON test
 	for {
@@ -63,5 +70,32 @@ func convDataStructToJson(data *shared.DataObj) []byte {
 // use it to play around...
 // note "Json" is not precise: includes type field aswell
 func constructDummyJson() []byte {
+	// construct DataObj that will be the actual JSON
+	// in this case construct info req:
+	dataObj := shared.DataObj{Id: "1337",
+							  Seq: 1337,
+							  Ts: "2018-10-23T 16:31:13.720069",
+							  Secret: "fancySecret"}
+
+	jsonB, err := json.Marshal(dataObj)
+	if err != nil {
+		fmt.Printf("Cannot Marshal %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("json encoded version of dataObj: ", string(jsonB))
+
+	// now take a byte slice add the type field 0x01
+	// needed: memory representation in big endian!
+	fmt.Println("length of json Bytes: ", len(jsonB))
+
+	b := make([]byte, len(jsonB) + 1)
+
+	binary.BigEndian.PutUint16(b[0:], 0x01)
+
+	fmt.Println("length of b: ", len(b))
+	fmt.Printf("content of b: % x", b)
+
+
 	return []byte("ShutUpGo")
 }
