0672fe7e3251956cccfc27cce9eba8c641ce7b76
monfron <simon.fronhoefer@gmail.com>
2018-10-25 11:11:10 +0200

    Implemented: Complete Json converting, Removed: Dummy funcs
    - We can now convert json -> struct, struct -> json vice versa.
    - I removed all the parts which are not used atm, in order not
    to loose the overview
    - in the next commit these parts are back
    
    Signed-off-by: monfron <simon.fronhoefer@gmail.com>

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -30,52 +30,21 @@ func runServer(port int, callSize int) {
 	ch := make(chan shared.ChResult)
 	fmt.Println(ch)
 
-	/* WIP removed during JSON test
-	tcpObj := serverProtos.NewTcpObj("TcpConn1", port, callSize)
-	tcpObj.Start(ch)
-	*/
+	dataObj := shared.DataObj{
+							  Type: 0x01, 
+							  Id: "1337",
+							  Seq: 1337,
+							  Ts: "2018-10-23T 16:31:13.720069",
+							  Secret: "fancySecret"}
 
-	/* WIP: disabled for reduced complexity
-	udpObj := serverProtos.NewUdpObj("UdpConn1")
-	udpObj.Start(ch)
-	*/
+	json := convDataStructToJson(&dataObj)
+	dataObRecovered := convJsonToDataStruct(json)
 
-	dummyJson := constructDummyJson()
-	dataObj := convJsonToDataStruct(dummyJson)
-	switch dataObj.Type {
-	case shared.INFO_REQUEST:
-		fmt.Println("INFO_REQUEST")
-	case shared.MEASUREMENT_START_REQUESTS:
-		fmt.Println("MEASUREMENT_START_REQUESTS")
-	case shared.MEASUREMENT_STOP_REQUEST:
-		fmt.Println("MEASUREMENT_STOP_REQUEST")
-	case shared.MEASUREMENT_INFO_REQUEST:
-		fmt.Println("MEASUREMENT_INFO_REQUEST")
-	case shared.TIME_DIFF_REQUEST:
-		fmt.Println("TIME_DIFF_REQUEST")
-	case shared.WARNING_ERR_MSG:
-		fmt.Println("WARNING_ERR_MSG")
-	default:
-		fmt.Printf("Unknown type")
-		os.Exit(1)
-
-	}
-
-	/* WIP removed during JSON test
-	for {
-		result := <- ch
-		fmt.Println("Server received from client: ", result)
-
-		// TODO: here will go the conJsonToDataStruct in the future
-		dataObj := conJsonToDataStruct(result.Json)
-
-		result.ConnObj.WriteAnswer([]byte("ServerReply"))
-	}
-	*/
+	fmt.Println("\ndataObj is: ", *dataObRecovered)
 }
 
 func convJsonToDataStruct(jsonData []byte) *shared.DataObj {
-	fmt.Printf("\nProcessing the following data: % x", jsonData)
+	fmt.Printf("\n Converting json data: % x", jsonData)
 
 	dataObj := new(shared.DataObj)
 
@@ -95,40 +64,24 @@ func convJsonToDataStruct(jsonData []byte) *shared.DataObj {
 }
 
 func convDataStructToJson(data *shared.DataObj) []byte {
-	fmt.Println("convert data struct to json (i.e. prepare outgoing msg")
-	return []byte("ShutUpGo")
-}
+	fmt.Println("\nConverting datastruct ", *data)
 
-
-
-
-// this is a shrinked version of convDataStructToJson
-// use it to play around...
-// note "Json" is not precise: includes type field aswell
-func constructDummyJson() []byte {
 	var resB []byte
-	// construct DataObj that will be the actual JSON
-	// in this case construct info req:
-	dataObj := shared.DataObj{Id: "1337",
-							  Seq: 1337,
-							  Ts: "2018-10-23T 16:31:13.720069",
-							  Secret: "fancySecret"}
 
-	jsonB, err := json.Marshal(dataObj)
+	// construct type field
+	typeB := make([]byte, 2)
+	binary.BigEndian.PutUint16(typeB[0:2], uint16(data.Type))
+
+	// ignore Type
+	data.Type = 0
+
+	jsonB, err := json.Marshal(data)
 	if err != nil {
 		fmt.Printf("Cannot Marshal %s\n", err)
 		os.Exit(1)
 	}
 
-	// now take a byte slice add the type field 0x01
-	typeB := make([]byte, 2)
-	binary.BigEndian.PutUint16(typeB[0:2], 0x01)
-
-	// add type field to result
 	resB = append(resB, typeB...)
-
-	// add json field to result
 	resB = append(resB, jsonB...)
-
 	return resB
 }
