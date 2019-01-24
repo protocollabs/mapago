39dc4f3301d2d38a7e79608966c4236bef2a083e
monfron <simon.fronhoefer@gmail.com>
2019-01-24 14:52:40 +0100

    Fixed & tested: Include final measurement data to msmtStopReply for UDP
    Closes: #38
    
    Signed-off-by: monfron <simon.fronhoefer@gmail.com>

--- measurement-plane/udp-throughput/server.go
+++ measurement-plane/udp-throughput/server.go
@@ -167,7 +167,9 @@ func (udpMsmt *UdpThroughputMsmt) udpServerWorker(closeCh <-chan interface{}, go
 }
 
 func (udpMsmt *UdpThroughputMsmt) CloseConn() {
-	var msmtData map[string]string
+	var mgmtData map[string]string
+	var msmtData []shared.DataResultObj
+	var combinedData shared.CombinedData
 
 	for streamId, conn := range udpMsmt.connStorage {
 		fmt.Println("\nUDP closing: ", streamId)
@@ -177,10 +179,27 @@ func (udpMsmt *UdpThroughputMsmt) CloseConn() {
 	msmtReply := new(shared.ChMsmt2Ctrl)
 	msmtReply.Status = "ok"
 
-	msmtData = make(map[string]string)
-	msmtData["msmtId"] = udpMsmt.msmtId
-	msmtData["msg"] = "all modules closed"
-	msmtReply.Data = msmtData
+	mgmtData = make(map[string]string)
+	mgmtData["msmtId"] = udpMsmt.msmtId
+	mgmtData["msg"] = "all modules closed"
+	combinedData.MgmtData = mgmtData
+
+	for c := 1; c <= udpMsmt.numStreams; c++ {
+		stream := "stream" + strconv.Itoa(c)
+
+		msmtStruct := udpMsmt.msmtInfoStorage[stream]
+
+		dataElement := new(shared.DataResultObj)
+		dataElement.Received_bytes = strconv.Itoa(int(msmtStruct.Bytes))
+		dataElement.Timestamp_first = msmtStruct.FirstTs
+		dataElement.Timestamp_last = msmtStruct.LastTs
+
+		msmtData = append(msmtData, *dataElement)
+		fmt.Println("\nmsmtData is: ", msmtData)
+	}
+
+	combinedData.MsmtData = msmtData
+	msmtReply.Data = combinedData
 
 	go func() {
 		udpMsmt.msmt2CtrlCh <- *msmtReply
