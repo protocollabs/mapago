12d874a31116de953f1fe60652ef73bf9151f66b
monfron <simon.fronhoefer@gmail.com>
2019-03-06 10:30:09 +0100

    Fixed: Client's tcp-throughput switched to bytecount
    - Compiles
    - But not tested atm
    - debug code going on
    
    Signed-off-by: monfron <simon.fronhoefer@gmail.com>

--- measurement-plane/tcp-throughput/client.go
+++ measurement-plane/tcp-throughput/client.go
@@ -7,25 +7,28 @@ import "strconv"
 import "fmt"
 import "github.com/protocollabs/mapago/control-plane/ctrl/shared"
 
-func NewTcpMsmtClient(config shared.ConfigurationObj, msmtStartRep *shared.DataObj, wg *sync.WaitGroup, closeConnCh <-chan string, callSize int, msmtByteStore map[string]*uint) {
+func NewTcpMsmtClient(config shared.ConfigurationObj, msmtStartRep *shared.DataObj, wg *sync.WaitGroup, closeConnCh <-chan string, callSize int, sentStreamBytes map[string]*uint, msmtTotalBytes uint) {
 	lAddr := config.Listen_addr
 	serverPorts := shared.ConvStrToIntSlice(msmtStartRep.Measurement.Configuration.UsedPorts)
+	workers, err  := strconv.ParseUint(config.Worker, 10, 32)
+	if err != nil {
+		fmt.Printf("\n Parseuint error: %s", err)
+		os.Exit(1)
+	}
 
-	// debug fmt.Println("\nbyte store is ", msmtByteStore)
+	streamBytes := msmtTotalBytes / uint(workers) 
+	// debug fmt.Println("\nevery stream has to sent: ", streamBytes)
 
 	for i, port := range serverPorts {
 		listen := lAddr + ":" + strconv.Itoa(port)
 		stream := "stream" + strconv.Itoa(i + 1)
-		
-		// debug fmt.Println("\nstream is: ", stream)
-		// debug fmt.Println("\nvalue is: ", msmtByteStore[stream])
 
 		wg.Add(1)
-		go tcpClientWorker(listen, wg, closeConnCh, callSize, msmtByteStore[stream])
+		go tcpClientWorker(listen, wg, closeConnCh, uint(callSize), sentStreamBytes[stream], streamBytes)
 	}
 }
 
-func tcpClientWorker(addr string, wg *sync.WaitGroup, closeConnCh <-chan string, callSize int, streamByteCtr *uint) {
+func tcpClientWorker(addr string, wg *sync.WaitGroup, closeConnCh <-chan string, callSize uint, sentStreamBytes *uint, streamBytes uint) {
 	buf := make([]byte, callSize, callSize)
 	
 	// debug fmt.Println("\nbyte ctr is:", *streamByteCtr)
@@ -48,15 +51,38 @@ func tcpClientWorker(addr string, wg *sync.WaitGroup, closeConnCh <-chan string,
 				os.Exit(1)
 			}
 		default:
+			// sent as long as "stream threshold" not reached
+			// case a) send whole callSize
+			if streamBytes >= callSize  {
+				bytes, err := conn.Write(buf)
+	
+				if err != nil {
+					fmt.Printf("\nWrite error: %s", err)
+					os.Exit(1)
+				}
 
-			bytes, err := conn.Write(buf)
-			if err != nil {
-				fmt.Printf("\nWrite error: %s", err)
-				os.Exit(1)
+				// update per stream counter
+				streamBytes -= uint(bytes)
+				// update stream counter reference for mapago-client => determine when its done
+				*sentStreamBytes = *sentStreamBytes + uint(bytes)
+			
+			// case b) last bytes to send are not a "full" buffer
+			} else if (streamBytes < callSize && streamBytes > 0) {
+				buf = make([]byte, streamBytes, streamBytes)
+				bytes, err := conn.Write(buf)
+	
+				if err != nil {
+					fmt.Printf("\nWrite error: %s", err)
+					os.Exit(1)
+				}
+
+				// update per stream counter
+				streamBytes -= uint(bytes)
+				// update stream counter reference for mapago-client => determine when its done
+				*sentStreamBytes = *sentStreamBytes + uint(bytes)
+
+			// case c): Default (streamBytes == 0 => enough sent) => Do nothing: Wait for channels
 			}
-
-			*streamByteCtr = *streamByteCtr + uint(bytes)
-
 		}
 	}
 }
